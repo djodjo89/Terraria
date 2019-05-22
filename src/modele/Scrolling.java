677379df918eedb88919950555bbfd3d6d1998fe
djodjo89 <simionmathys@yahoo.fr>
2019-05-22 21:15:48 +0200

    Ajout d'un petit commentaire pour le scrolling !

--- src/modele/Scrolling.java
+++ src/modele/Scrolling.java
@@ -8,7 +8,7 @@ public class Scrolling {
 	private int xTranslate;
 	private Pane pane;
 	private Pane paneMap;
-	
+
 	public Scrolling(Pane pane,Pane paneMap) {
 		this.xTranslate = 0;
 		this.paneMap=paneMap;
@@ -22,5 +22,21 @@ public class Scrolling {
 			this.xTranslate+=10;
 		pane.setTranslateX(xTranslate);
 	}
+	
+	// Une version du scroll avec les vecteurs
+	
+	/*public void futurScroll (Vecteur vecteur, Personnage perso) {
+		
+		if (vecteur.getX() > 0 && perso.getX() > this.paneMap.getWidth() / 2 && 1.5 * this.paneMap.getWidth() > perso.getX())
+			
+			this.xTranslate -= perso.getVitesseX() ;
+		
+		else if (vecteur.getX() < 0 && this.xtranslate < 0 && this.paneMap.getWidth() - this.paneMap.getWidth() / 2)
+			
+			this.xTranslate += perso.getVitesseX() ;
+		
+		this.pane.setTranslateX(this.xTranslate);
+		
+	}*/
 
 }
